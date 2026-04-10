f777f85729cef7995e93d635a5f103b11552b1c0
sangeetmohan <mailsangeetmohan@gmail.com>
2026-04-10 11:08:18 -0400

    Create requirements file that includes libraries

---
+++ regression.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
